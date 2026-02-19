8ef70e9fdd7f010573858eebf25595020fe91da0
Mansi <mansi@gmail.com>
2026-02-19 18:24:00 +0530

    added new changes

--- src/components/AlbumsBox.tsx
+++ src/components/AlbumsBox.tsx
@@ -41,7 +41,7 @@ export default function AlbumsBox({
 
             <div className="absolute top-0 -right-[140%] 3xl:h-[290px] 3xl:w-[290px] 2xl:h-[260px] 2xl:w-[260px] xl:h-[220px] xl:w-[220px] lg:h-[160px] lg:w-[160px] 3xl:block 2x:block xl:block lg:block none bg-[#00000066] group-hover:right-0 transition-all duration-700" />
 
-            <h5 className="absolute 3xl:-bottom-[20%] 2xl:-bottom-[20%] xl:-bottom-[28%] lg:-bottom-[20%] bottom-0 left-[2%] 3xl:group-hover:bottom-0 2xl:group-hover:bottom-0 xl:group-hover:bottom-0 lg:group-hover:bottom-0 transition-all duration-700 p-2 3xl:text-[20px] 2xl:text-[18px] xl:text-[16px] lg:text-[16px] md:text-[15px] text-[14px] leading-tight font-semibold">
+            <h5 className="absolute hidden 3xl:-bottom-[20%] 2xl:-bottom-[20%] xl:-bottom-[28%] lg:-bottom-[20%] bottom-0 left-[2%] 3xl:group-hover:bottom-0 2xl:group-hover:bottom-0 xl:group-hover:bottom-0 lg:group-hover:bottom-0 transition-all duration-700 p-2 3xl:text-[20px] 2xl:text-[18px] xl:text-[16px] lg:text-[16px] md:text-[15px] text-[14px] leading-tight font-semibold">
               {album.title}
             </h5>
           </div>
